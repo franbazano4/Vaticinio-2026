1d2d591ee40e2085a3935834846109c09fa3e435
franbazano4 <franbazano@gmail.com>
2026-06-24 01:33:03 -0300

    Actualizar index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import resultsRouter from "./results";
+import probabilitiesRouter from "./probabilities";
 
 const router: IRouter = Router();
 router.use(healthRouter);
 router.use(resultsRouter);
+router.use(probabilitiesRouter);
 
 export default router;
